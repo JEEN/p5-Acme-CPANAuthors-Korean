952be044658e94b91a1fe55fd94341877bed6228
JEEN Lee <jeen@perl.kr>
2009-10-07 15:13:40 +0900

    Checking in changes prior to tagging of version 0.02.  Changelog diff is:
    diff --git a/Changes b/Changes
    index 13c44cc..24cb57c 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,4 +1,7 @@
     Revision history for Perl extension Acme::CPANAuthors::Korean
    
    +0.02    Wed Oct  7 15:13:00 2009
    +        - added KHS
    +
     0.01    Wed Oct  7 14:32:49 2009
             - original version

--- lib/Acme/CPANAuthors/Korean.pm
+++ lib/Acme/CPANAuthors/Korean.pm
@@ -2,13 +2,14 @@ package Acme::CPANAuthors::Korean;
 
 use strict;
 use warnings;
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use Acme::CPANAuthors::Register (
 	JEEN => "Jong-jin Lee",
 	AERO => "C.H. Kang",
 	KEEDI => "Keedi Kim",
 	YOU => "YOU Hyun Jo",
+    KHS => "HyeonSeung Kim"
 );
 
 1;
