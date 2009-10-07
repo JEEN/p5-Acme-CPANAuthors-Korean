a81db46c5999b17378523ac06c8d64ac02af817a
JEEN Lee <jeen@perl.kr>
2009-10-07 15:08:08 +0900

    Checking in changes prior to tagging of version 0.01.  Changelog diff is:

--- lib/Acme/CPANAuthors/Korean.pm
+++ lib/Acme/CPANAuthors/Korean.pm
@@ -4,20 +4,46 @@ use strict;
 use warnings;
 our $VERSION = '0.01';
 
+use Acme::CPANAuthors::Register (
+	JEEN => "Jong-jin Lee",
+	AERO => "C.H. Kang",
+	KEEDI => "Keedi Kim",
+	YOU => "YOU Hyun Jo",
+);
+
 1;
+
 __END__
+
+=encoding utf-8
 
 =head1 NAME
 
-Acme::CPANAuthors::Korean -
+Acme::CPANAuthors::Korean - We are Korean CPAN Authors!
 
 =head1 SYNOPSIS
 
+  use Acme::CPANAuthors;
   use Acme::CPANAuthors::Korean;
+  $authors = Acme::CPANAuthors->new('Korean');
+
+  $number   = $authors->count;
+  @ids      = $authors->id;
+  @distors  = $authors->distributions('JEEN');
+  $url      = $authors->avatar_url('KEEDI');
+  $kwalitee = $authors->kwalitee('AERO');
 
 =head1 DESCRIPTION
 
-Acme::CPANAuthors::Korean is
+See documentation for L<Acme::CPANAuthors> for more details.
+
+=head1 DEPENDENCIES
+
+L<Acme::CPANAuthors>
+
+=head1 DEVELOPMENT
+
+Git repository: http://github.com/jeen/Acme-CPANAuthors-Korean/
 
 =head1 AUTHOR
 
