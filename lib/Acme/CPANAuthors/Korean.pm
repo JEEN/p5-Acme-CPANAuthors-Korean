322e66c7e1a84392dbbdc0b5ae8428894a6bac14
JEEN Lee <jeen@perl.kr>
2009-10-07 14:33:12 +0900

    generated skeleton of Acme::CPANAuthors::Korean

---
+++ lib/Acme/CPANAuthors/Korean.pm
@@ -0,0 +1,33 @@
+package Acme::CPANAuthors::Korean;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Acme::CPANAuthors::Korean -
+
+=head1 SYNOPSIS
+
+  use Acme::CPANAuthors::Korean;
+
+=head1 DESCRIPTION
+
+Acme::CPANAuthors::Korean is
+
+=head1 AUTHOR
+
+JEEN E<lt>jeen@perl.krE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
