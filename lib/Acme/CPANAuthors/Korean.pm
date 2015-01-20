019e97d5cf6ef7533cb4934bc649d677039c4336
Jeen Lee <aiatejin@gmail.com>
2015-01-20 14:03:57 +0900

    Checking in changes prior to tagging of version 0.15.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 1037d71..06358c0 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,9 @@ Revision history for Perl extension Acme-CPANAuthors-Korean
    
     {{$NEXT}}
    
    +0.15 2015-01-20T05:03:46Z
    +        - Add RAKJIN
    +
     0.14 2013-12-31T03:04:32Z
             - Add ISJOUNG

--- lib/Acme/CPANAuthors/Korean.pm
+++ lib/Acme/CPANAuthors/Korean.pm
@@ -2,7 +2,7 @@ package Acme::CPANAuthors::Korean;
 
 use strict;
 use warnings;
-our $VERSION = '0.14';
+our $VERSION = '0.15';
 use utf8;
 
 use Acme::CPANAuthors::Register (
@@ -20,6 +20,7 @@ use Acme::CPANAuthors::Register (
     SKYEND   => "J.W. Han",
     YONGBIN  => "Yongbin Yu",
     YOU      => "YOU Hyun Jo",
+    RAKJIN   => "Rakjin Hwang",
 );
 
 1;
